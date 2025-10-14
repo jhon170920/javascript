78b818c7d14121bcfa96005192749459c3960df8
jhon170920 <j276scc@gmail.comclear>
2025-10-14 17:16:08 -0500

    subiendo archivos

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function mostrarMensaje(){
+    alert("Bienvenido al curso practico de javascript");
+}
+
+/* array a mayuscula
+let arr = ["a","b","c","d"]
+
+for(i = 0; i < arr.length; i++){
+    arr[i] = arr[i].toUpperCase();
+    console.log(arr[i]);
+} 
+function lo(){
+    x
+}
+
+function datos(){
+let nombre = prompt("Ingrese su nombre");
+let edad = prompt("Ingrese su edad");
+let profesion = prompt("Ingrese su profesion");
+
+console.log("Bienvenido "+nombre+" al curso de java, tiene "+edad+" años y eres "+profesion);
+}
+*/
